dc10b8fbd6ba5b5aaa96fd8a5a734d6b90bb7d2a
saikiranreddyenugala <77818423+saikiranreddyenugala@users.noreply.github.com>
2022-01-28 14:50:21 -0600

    added reducer,py and sorter.py

---
+++ sorter.py
@@ -0,0 +1,12 @@
+# This is sai kiran's sorter
+
+n = open("out01.txt","r")  # open file, read-only
+s = open("sortOutput.txt", "w") # open file, write
+lines = n.readlines()
+lines.sort()
+
+for line in lines:
+ s.write(line)
+
+n.close()
+s.close()
